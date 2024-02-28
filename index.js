fb93d48a26e9ed8f2efcc4864e2d26c0e66ef11f
Sain Bux <sainbuxdev@gmail.com>
2024-02-28 07:27:46 -0800

    Update

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+/*
 app.get('/', (req, res) => {
 
         res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+*/
+app.use(express.static(path.join(__dirname,'public')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {console.log(`Server is started on port ${PORT}`)});
