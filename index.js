9e833846f9b5eac7139331d7a7ff0a7d3271950c
Sain Bux <sainbuxdev@gmail.com>
2024-02-28 07:09:55 -0800

    Update

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+
+        res.send("<h1>Hello Ayan</h1>")
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {console.log(`Server is started on port ${PORT}`)});
